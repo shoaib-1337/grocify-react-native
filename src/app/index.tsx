09f1066cc3038c6fa1c295fd77150d7f756be546
Shoaib Ali <sali18089@gmail.com>
2026-04-20 14:45:39 +0500

    Project config and tailwind setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500 bg-purple-500">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
