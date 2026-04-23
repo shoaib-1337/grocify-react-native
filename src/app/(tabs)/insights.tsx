6790791f8da003cb866bd04ce659de60e03a03c7
Shoaib Ali <sali18089@gmail.com>
2026-04-23 14:52:13 +0500

    App Completed

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -2,7 +2,7 @@ import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
 import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
 import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
 import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
-import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
+// import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
 import UserProfile from "@/components/insights/UserProfile";
 import TabScreenBackground from "@/components/TabScreenBackground";
 import { ScrollView } from "react-native";
@@ -25,7 +25,7 @@ const InsightsScreen = () => {
         <ClearCompletedButton />
       </ScrollView>
 
-      <SentryFeedbackButton />
+      {/* <SentryFeedbackButton /> */}
     </>
   );
 };
